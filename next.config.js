240125b7739a14f72d66336f0556fad3537e0444
Erickson Manuel Holguin <erickson01d@gmail.com>
2022-05-04 14:14:27 -0400

    updating next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: "build",
-  trailingSlash: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  experimental: {
-    outputStandalone: true,
-  },
 };
 
 module.exports = nextConfig;
